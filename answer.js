9c14bb4adaca5de2b8d2713e53fda6683c3c2525
Lorn <laurynnryerse@gmail.com>
2019-12-02 16:55:06 -0700

    done

--- answer.js
+++ answer.js
@@ -1,1 +1,19 @@
 // *** YOUR ANSWER BELOW ***
+
+function first(word){
+    return "super" + word;
+}
+console.log(first("lame"));
+
+function secondary(word){
+    return "duper" + word;
+}
+console.log(first(secondary(" not fun")));
+
+function ET(word){
+    return "Uber " + word;
+}
+
+console.log(ET(first(secondary("cool"))));
+
+// I know I am not supposed to capitalize the first letter in a function, but I couldn't help myself.
